3c1dfa136140959ca3af28c92a06710ce760feb6
Gabriel Ribeiro <gaabriel.jrb@gmail.com>
2022-10-12 18:12:24 -0300

    class 12 - Static Site Generation

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { GetServerSideProps } from "next";
+import { GetStaticProps } from "next";
 import Head from "next/head";
 import { SubscribeButton } from "../components/SubscribeButton";
 import { stripe } from "../services/stripe";
@@ -38,7 +38,7 @@ export default function Home({ product }: HomeProps) {
   );
 }
 
-export const getServerSideProps : GetServerSideProps = async () => {
+export const getStaticProps : GetStaticProps = async () => {
   const price = await stripe.prices.retrieve('price_1LcTh5KJlznDBm5j9f6yNYrK');
   // {
   //   expand: ['product'] //pega todas as infos do produto
@@ -55,6 +55,7 @@ export const getServerSideProps : GetServerSideProps = async () => {
   return {
     props: {
       product
-    }
+    },
+    revalidate: 60 * 60 * 24, // 24 hours
   }
 }
