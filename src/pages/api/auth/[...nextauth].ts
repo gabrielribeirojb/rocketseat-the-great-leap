1dde3cd2cc77811298973ca99031f5afd1de979d
Gabriel Ribeiro <gaabriel.jrb@gmail.com>
2022-10-31 09:03:44 -0300

    class 17 - storing a user in the database

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,8 @@
+import { query as q } from 'faunadb';
 import NextAuth from "next-auth";
 import GithubProvider from "next-auth/providers/github";
+
+import { fauna } from '../../../services/fauna';
 
 export const authOptions = {
     // Configure one or more authentication providers
@@ -15,6 +18,28 @@ export const authOptions = {
       })
       // ...add more providers here
     ],
-  }
-  export default NextAuth(authOptions);
+    jwt: {
+      signingKey: process.env.SIGNING_KEY
+    },
+    callbacks: {
+      async signIn({ user }) {
+        const { email } = user;
+
+        try {
+          await fauna.query(
+            q.Create(
+              q.Collection('users'),
+              { data: { email }}
+            )
+          )
+          return true;
+
+        } catch {
+          return false;
+        }
+      },
+    }
+}
+
+export default NextAuth(authOptions);
 
