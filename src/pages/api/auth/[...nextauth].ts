0c40a8c9f6da3cafa8fc9ee112401b07a6f20084
Gabriel Ribeiro <gaabriel.jrb@gmail.com>
2022-10-31 10:05:04 -0300

    class 19 - checking if user is duplicated

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -24,13 +24,29 @@ export const authOptions = {
 
         try {
           await fauna.query(
-            q.Create(
-              q.Collection('users'),
-              { data: { email }}
+            q.If(
+              q.Not(
+                q.Exists(
+                  q.Match(
+                    q.Index('user_by_email'),
+                    q.Casefold(user.email)
+                  )
+                )
+              ),
+                q.Create(
+                  q.Collection('users'),
+                  { data: { email }}
+                ),
+                q.Get(
+                  q.Match(
+                    q.Index('user_by_email'),
+                    q.Casefold(user.email)
+                  )
+                )
+              )
+                  
             )
-          )
-          return true;
-
+              return true;
         } catch {
           return false;
         }
